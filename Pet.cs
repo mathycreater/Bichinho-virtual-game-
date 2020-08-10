07abe751d9756ee975cdc7fabb7f1eb8ce70dd2e
Matheus Corrêa <46695762+mathycreater@users.noreply.github.com>
2020-08-09 22:18:22 -0300

    Add files via upload

---
+++ Pet.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProgramaLalitinchacha
+{
+    public class Pet
+    {
+        public int Vitoria { get; set; }
+        public string Nome { get; set; }
+        public string Tipo { get; set; }
+        public int poder { get; set; }
+        public void Lutar(Pet A, Pet B)
+        {
+            if (A.poder > B.poder)
+            {
+                Console.WriteLine(A.Nome + " Venceu!");
+                A.Vitoria += 1;
+            }
+            else if (A.poder == B.poder)
+            {
+                Console.WriteLine("Empatou");
+            }
+            else
+            {
+                Console.WriteLine(B.Nome + " Venceu!");
+                B.Vitoria += 1;
+            }
+        }
+    }
+
+}
---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace ProgramaLalitinchacha
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            bool A = false;
+            Pet Pet1 = new Pet();
+            Pet Pet2 = new Pet();
+
+            Console.WriteLine("Digite o nome do primeiro pet: ");
+            Pet1.Nome = Console.ReadLine();
+
+            Console.WriteLine("Digite o nome do segundo pet: ");
+            Pet2.Nome = Console.ReadLine();
+
+            while  (A == false)
+
+            {
+                Console.WriteLine("Digite a força do primeiro Pet: ");
+                Pet1.poder = int.Parse(Console.ReadLine());
+
+                Console.WriteLine("Digite o nome do seu segundo Pet: ");
+                Pet2.poder = int.Parse(Console.ReadLine());
+
+                Pet1.Lutar(Pet1, Pet2);
+
+                Console.WriteLine("Quantidade de vitórias de " + Pet1.Nome + ": " + Pet1.Vitoria);
+                Console.WriteLine("Quantidade de vitórias de " + Pet2.Nome + ": " + Pet2.Vitoria);
+
+            }
+        }
+    }
+}
